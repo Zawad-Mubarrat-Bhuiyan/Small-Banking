d071b99bf5daf936f30825d1ecd5643baf09950e
Zawad-Mubarrat-Bhuiyan <zawadbhuiyan@gmail.com>
2021-08-29 11:15:40 +0600

    third commit

--- js/banking.js
+++ js/banking.js
@@ -23,3 +23,30 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     //clear the deposit input field
     depositInput.value = '';
 })
+
+//handle withdraw event handler
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    //get the withdraw amount
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawText);
+
+    //update withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawAmount = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawAmount + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    //update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    //clear withdraw input
+    withdrawInput.value = '';
+
+})
